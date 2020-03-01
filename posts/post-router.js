7bbfb3c09d4eef4b789fad5ee87a8b815072aba0
vy3191 <venkyafhs@gmail.com>
2020-03-01 16:53:28 -0500

    Added get method

--- posts/post-router.js
+++ posts/post-router.js
@@ -4,23 +4,45 @@ const db = require("../data/config")
 const router = express.Router()
 
 router.get("/", async (req, res, next) => {
-
+      try {
+         // same: SELECT * FROM "posts";
+        const posts = await db.select("*").from("posts");
+        res.status(200).json(posts);
+      } catch(err) {
+         next(err);
+      }
 })
 
 router.get("/:id", async (req, res, next) => {
+      try {
 
+      } catch(err) {
+         next(err);
+      }
 })
 
 router.post("/", async (req, res, next) => {
+      try {
 
+      } catch(err) {
+         next(err);
+      }
 })
 
 router.put("/:id", async (req, res, next) => {
+      try {
 
+      } catch(err) {
+         next(err);
+      }
 })
 
 router.delete("/:id", async (req, res, next) => {
+      try {
 
+      } catch(err) {
+         next(err);
+      }
 })
 
 module.exports = router
